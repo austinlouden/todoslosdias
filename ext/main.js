5d9e7355b5096d2f4bb50b81d2274d2ff1bdd2a2
Austin Louden <austinlouden@gmail.com>
2015-06-25 22:09:42 +0200

    add tooltips

--- ext/main.js
+++ ext/main.js
@@ -10,12 +10,16 @@ document.addEventListener('DOMContentLoaded', function() {
         var object = results[i];
         var imageFile = object.get('picture');
         var imageURL = imageFile.url();
-        console.log(imageURL);
         document.getElementById('spanish').textContent = object.get('spanish');
         document.getElementById('english').textContent = object.get('english');
         document.getElementById('speech').textContent = object.get('speech');
         document.getElementById('ex_span').textContent = object.get('ex_span');
         document.getElementById('image').src = imageURL;
+        document.getElementById('ex_span').title = object.get('ex_eng');
+
+        $(function () {
+          $('[data-toggle="tooltip"]').tooltip()
+        }) 
       }
     },
     error: function(error) {
